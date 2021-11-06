575e57ea095326d4089124aaf2550b66a26b4a36
LiviaDita <liviaditaekapuspita@gmail.com>
2021-11-06 18:14:37 +0700

    Belajar Intent
    latihan intent dengan 4 tombol dan 1 teks

---
+++ app/src/main/java/com/dita/intentliviadita/MoveWithDataActivity.kt
@@ -0,0 +1,25 @@
+package com.dita.intentliviadita
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MoveWithDataActivity : AppCompatActivity() {
+    private lateinit var tvDataReceived: TextView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_move_with_data)
+
+        tvDataReceived = findViewById(R.id.tv_data_received)
+        DataReceived()
+    }
+
+    private fun DataReceived() {
+        val bundle = intent.extras
+        val nama = bundle?.getString("Nama")
+        val alamat = bundle?.getString("Alamat")
+
+        tvDataReceived.text = nama + " " + alamat
+    }
+}
