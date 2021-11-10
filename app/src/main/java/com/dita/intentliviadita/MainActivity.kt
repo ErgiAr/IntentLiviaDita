b5d451b864e7d630436c6e52e40338254980b934
LiviaDita <liviaditaekapuspita@gmail.com>
2021-11-10 15:01:45 +0700

    Intent Livia Dita
    Lanjutan latihan intent 4 button dan 1 text

--- app/src/main/java/com/dita/intentliviadita/MainActivity.kt
+++ app/src/main/java/com/dita/intentliviadita/MainActivity.kt
@@ -1,14 +1,19 @@
 package com.dita.intentliviadita
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var btnMoveActivity: Button
     private lateinit var btnMoveWithData: Button
+    private lateinit var btnDialNumber: Button
+    private lateinit var btnResultFromActivity: Button
+    private lateinit var tvResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,6 +24,15 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         btnMoveWithData = findViewById(R.id.btn_move_with_data)
         btnMoveWithData.setOnClickListener(this)
+
+        btnDialNumber = findViewById(R.id.btn_dial_number)
+        btnDialNumber.setOnClickListener(this)
+
+        btnResultFromActivity = findViewById(R.id.btn_result_from_activity)
+        btnResultFromActivity.setOnClickListener(this)
+
+        tvResult = findViewById(R.id.tv_result)
+        WarnaReceived()
     }
 
     override fun onClick(v: View?) {
@@ -36,7 +50,22 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
                     intent.putExtras(bundle)
                     startActivity(intent)
                 }
+                R.id.btn_dial_number -> run {
+                    var dialNumber = "088234039767"
+                    val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + dialNumber))
+                    startActivity(intent)
+                }
+                R.id.btn_result_from_activity -> {
+                    val intent = Intent(this, MoveForResultActivity::class.java)
+                    startActivity(intent)
+                }
             }
         }
+    }
+
+    private fun WarnaReceived() {
+        val bundle = intent.extras
+        val warna = bundle?.getString("Warna")
+        tvResult.text = warna
     }
 }
--- app/src/main/java/com/dita/intentliviadita/MoveWithDataActivity.kt
+++ app/src/main/java/com/dita/intentliviadita/MoveWithDataActivity.kt
@@ -1,18 +1,37 @@
 package com.dita.intentliviadita
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import android.widget.TextView
 import org.w3c.dom.Text
 
-class MoveWithDataActivity : AppCompatActivity() {
+class MoveWithDataActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var tvDataReceived: TextView
+    private lateinit var btnback: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_move_with_data)
 
         tvDataReceived = findViewById(R.id.tv_data_received)
         DataReceived()
+
+        btnback = findViewById(R.id._back)
+        btnback.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id._back -> run {
+                    val intent = Intent(this, MainActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+        }
     }
 
     private fun DataReceived() {
